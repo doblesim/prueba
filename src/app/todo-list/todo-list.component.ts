167213b9d4fc9e8cb17a1c500d4407b7dd015b45
Jorge Medrano <doblesim.es@gmail.com>
2020-05-10 10:54:45 +0200

    Principio ejercicio 1

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  template: '<h1>Todo List</h1>',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
